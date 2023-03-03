4a393f4fc05d24bb48584040a2d56fb60ddd12a2
KOUSHIK SARKAR <>
2023-03-03 17:29:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+//MovieCollectionRunner.java
+
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String arg[])
+    {
+
+        MovieCollection myCollection = new MovieCollection("movies_data.csv");
+        myCollection.menu();
+
+    }
+}
+ 
